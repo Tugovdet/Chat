7500dc46c0275bf207e37a7a9161b354c559f617
Anton Korneyev <an.korn.zasekin@gmail.com>
2017-05-08 20:06:48 +0400

    Added Chat class

--- components/chat/chat.js
+++ components/chat/chat.js
@@ -2,8 +2,29 @@
   'use strict';
 
   class Chat {
-    constructor({ el }) {
+    constructor({ el, data = { messages: [] } }) {
       this.el = el;
+      this.data = data;
+    }
+
+    render() {
+      const messagesHTML = this.data.messages.map(mData =>
+        `
+          <div class='chat__message'>
+            <span class='chat__author'>${mData.username}</span>
+            ${mData.message}
+          </div>
+        `).join('');
+
+      this.el.innerHTML = `
+        <div class='chat'>
+          ${messagesHTML}
+        </div>
+      `;
+    }
+
+    addMessage(message) {
+      this.data.messages.push(message);
     }
   }
 
