7a94594cb01a8f18202edc550db5ccc8fa04188e
Anton Korneyev <an.korn.zasekin@gmail.com>
2017-05-08 19:48:54 +0400

    Added render() to Form class

--- components/form/form.js
+++ components/form/form.js
@@ -1,1 +1,22 @@
-'use strict';
+(function () {
+  'use strict';
+
+  class Form {
+    constructor({ el }) {
+      this.el = el;
+    }
+
+    render() {
+      this.el.innerHTML = `
+      <form>
+        <textarea name='message' rows=4></textarea>
+        <br/>
+        <input type='submit'/>
+      </form>    
+      `;
+    }
+  }
+
+  // export
+  window.Form = Form;
+}());
