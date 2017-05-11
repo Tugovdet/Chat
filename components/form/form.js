da83376b9638a04f9dfd0568243999ae83f124a0
Anton Korneyev <an.korn.zasekin@gmail.com>
2017-05-11 07:25:01 +0400

    Added _getFormData() to Form

--- components/form/form.js
+++ components/form/form.js
@@ -2,17 +2,44 @@
   'use strict';
 
   class Form {
-    constructor({ el }) {
-      this.el = el;
+    constructor(options) {
+      this.el = options.el;
+
+      this._onSubmit = this._onSubmit.bind(this);
+      this._initEvents();
+    }
+
+    onSubmit(cb = () => {}) {
+      this._submitCallback = cb;
+    }
+
+    _initEvents() {
+      this.addEventListener('submit', this._onSubmit, false);
+    }
+
+    _getFormData() {
+      const message = Document.querySelector('textarea');
+
+      const formData = { message };
+
+      return formData;
+    }
+
+    _onSubmit(ev) {
+      ev.preventDefault();
+
+      const formData = this._getFormData();
+      this.onSubmit(formData);
     }
 
     render() {
       this.el.innerHTML = `
-      <form>
-        <textarea name='message' rows=4></textarea>
-        <br/>
-        <input type='submit'/>
-      </form>    
+        <input name="username" required placeholder="Username" />
+        <form class="form">
+          <textarea name="message" required placeholder="Message" rows=4></textarea>
+          <br/>
+          <input type="submit"/>
+        </form>
       `;
     }
   }
